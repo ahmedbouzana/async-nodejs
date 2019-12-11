1dd7b165384619f268aa92de2aadc7755139e69c
ahmedbouzana <ah.bouzana@gmail.com>
2019-12-11 22:46:29 +0100

    Callback

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-console.log("before")
+console.log("before");
 
-const user = getUser(1);
-console.log(user);
+getUser(1, function(user) {
+  console.log("User", user);
+
+  getRepositories(user.githubUsername, repos => {
+    console.log("Repos", repos);
+  });
+});
 
 console.log("after");
 
-function getUser(id){
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading user from database...");
+    callback({ id: id, githubUsername: "ahmedbouzana" });
+  }, 2000);
+}
 
-    setTimeout(()=>{
-        console.log("Reading user from database...")
-        return{id:id, githubUsername: "ahmedbouzana"};
-    }, 2000);
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("Calling GitHub API...");
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
 }
